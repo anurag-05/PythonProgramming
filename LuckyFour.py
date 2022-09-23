623598bb625b349c40ae6727cda1234fb844bd4c
Anurag Nagarkoti <anujnagarkoti@gmail.com>
2022-09-23 21:02:40 +0200

    Practice program #1

---
+++ LuckyFour.py
@@ -0,0 +1,11 @@
+num = []
+for i in range(int(input())):
+    element = input()
+    count = 0
+    for j in range(0,len(element)): #iterate over the string
+        res = element[j]
+        if (element[j] == '4'):
+            count+= 1
+    num.insert(i, count)
+
+print(*num, sep = "\n")    #Splat operator | prints list without loops | def valye sep = ' '
